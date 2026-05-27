83c0e5f5e037a86631e985a4a9e2f95967ff89f1
santiago carmona <santiagocarmonacanas@gmail.com>
2026-05-27 09:18:44 -0500

    Primer montaje de empleado comun

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { useAuth } from './context/AuthContext'
 import Layout from './components/Layout'
-import Login from './pages/Login'
+import Login from './pages/login'
 import Dashboard from './pages/Dashboard'
 import Chat from './pages/Chat'
 import Incidentes from './pages/Incidentes'
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
-import './index.css'
+import './styles/globals.css'
+import './App.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
